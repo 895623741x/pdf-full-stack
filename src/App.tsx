a338d4d931296e2257c4f44c195d9310774af3a4
895623741x <tommyhsu69@gmail.com>
2022-09-25 23:01:24 -0700

    finish upload zone with preview section and upload multiple files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {Route, Routes} from "react-router-dom";
 import Login from "./pages/login/Login";
 import Upload from "./pages/upload/Upload";
 
 function App() {
+    const [newUserInfo, setNewUserInfo] = useState<Object>({
+        profileImages: [],
+    });
+
+    const updateUploadedFiles = (files: File[]) => setNewUserInfo({...newUserInfo, profileImages: files});
+
     return (
         <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/upload" element={<Upload />} />
+            <Route
+                path="/upload"
+                element={
+                    <Upload
+                        accept=".jpg,.png,.jpeg"
+                        label="Profile Image(s)"
+                        multiple
+                        updateFilesCb={updateUploadedFiles}
+                    />
+                }
+            />
         </Routes>
     );
 }
--- src/pages/upload/Upload.tsx
+++ src/pages/upload/Upload.tsx
@@ -18,6 +18,7 @@ interface props {
     updateFilesCb?: any;
     maxFileSizeInBytes?: number;
     multiple?: any;
+    accept?: string;
 }
 
 const DEFAULT_MAX_FILE_SIZE_IN_BYTES = 500000;
@@ -31,19 +32,66 @@ const FileUpload = ({
     maxFileSizeInBytes = DEFAULT_MAX_FILE_SIZE_IN_BYTES,
     ...otherProps
 }: props) => {
-    const fileInputField = useRef(null);
+    const fileInputField = useRef<any>(null);
     const [files, setFiles] = useState<any>({});
+
+    const handleUploadBtnClick = () => {
+        fileInputField.current.click();
+    };
+
+    const handleNewFileUpload = (e: any) => {
+        const {files: newFiles} = e.target;
+        if (newFiles.length) {
+            let updatedFiles = addNewFiles(newFiles);
+            setFiles(updatedFiles);
+            callUpdateFilesCb(updatedFiles);
+        }
+    };
+    // The addNewFiles function takes a FileList object (e.target.files above returns a FileList), iterates through it, and returns an object where the key is the file name and the value is the File object.
+    const addNewFiles = (newFiles: File[]) => {
+        for (let file of newFiles) {
+            if (file.size <= maxFileSizeInBytes) {
+                if (!otherProps.multiple) {
+                    return {file};
+                }
+                files[file.name] = file;
+            }
+        }
+        return {...files};
+    };
+
+    // Since React state updates are asynchronous, there is no guarantee that when the callUpdateFilesCb gets called, the files state will have changed.
+    const convertNestedObjectToArray = (nestedObj: any) => Object.keys(nestedObj).map((key) => nestedObj[key]);
+
+    const callUpdateFilesCb = (files: File[]) => {
+        const filesAsArray = convertNestedObjectToArray(files);
+        updateFilesCb(filesAsArray);
+    };
+
+    // The removeFile function will take a file name, delete it from the files state, update the files state, and inform the parent component of the changes.
+    const removeFile = (fileName: string) => {
+        delete files[fileName];
+        setFiles({...files});
+        callUpdateFilesCb({...files});
+    };
 
     return (
         <>
             <FileUploadContainer>
                 <InputLabel>{label}</InputLabel>
                 <DragDropText>Drag and drop your files anywhere or</DragDropText>
-                <UploadFileBtn type="button">
+                <UploadFileBtn type="button" onClick={handleUploadBtnClick}>
                     <i className="fas fa-file-upload" />
                     <span> Upload {otherProps.multiple ? "files" : "a file"}</span>
                 </UploadFileBtn>
-                <FormField type="file" ref={fileInputField} title="" value="" {...otherProps} />
+                <FormField
+                    type="file"
+                    ref={fileInputField}
+                    onChange={handleNewFileUpload}
+                    title=""
+                    value=""
+                    {...otherProps}
+                />
             </FileUploadContainer>
             <FilePreviewContainer>
                 <span>To Upload</span>
@@ -61,7 +109,10 @@ const FileUpload = ({
                                         <span>{file.name}</span>
                                         <aside>
                                             <span>{convertBytesToKB(file.size)} kb</span>
-                                            <RemoveFileIcon className="fas fa-trash-alt" />
+                                            <RemoveFileIcon
+                                                className="fas fa-trash-alt"
+                                                onClick={() => removeFile(fileName)}
+                                            />
                                         </aside>
                                     </FileMetaData>
                                 </div>
